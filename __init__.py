828332bbaafaaef57692d272cd9f05882335af18
klinter007 <131816305+klinter007@users.noreply.github.com>
2024-03-04 23:16:11 +0700

    fix

---
+++ __init__.py
@@ -0,0 +1,24 @@
+# Import local node class modules
+from .concat_klinter import concat_klinter
+from .concat_klinter import whitelist
+
+# Consider adding more imports here as necessary
+
+# Mapping of node identifiers to their respective class implementations
+NODE_CLASS_MAPPINGS = {
+    "concat_klinter": concat_klinter,
+    "whitelist": whitelist,
+    
+    # You can add more node mappings here
+}
+
+# Mapping of node identifiers to their display names for UI or logging purposes
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "concat_klinter": "Concat String (Klinter)",
+    "whitelist": "Whitelist (Klinter)",
+    
+    # Additional display names can be added here
+}
+
+# Define what symbols this module exports
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
