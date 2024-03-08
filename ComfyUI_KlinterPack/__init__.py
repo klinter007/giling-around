7e104dbc28b6e2c3ee823f5c8ea9232ed2f7953b
klinter007 <131816305+klinter007@users.noreply.github.com>
2024-03-09 00:24:20 +0700

    latest
    concat done
    filter done
    single string done
    size selector done
    show string wup

--- ComfyUI_KlinterPack/__init__.py
+++ ComfyUI_KlinterPack/__init__.py
@@ -1,2 +1,30 @@
-from .concat_klinter import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+# Import local node class modules
+from .nodes import concat
+from .nodes import Filter
+from .nodes import PresentString
+from .nodes import SizeSelector
+from .nodes import SingleString
+# Consider adding more imports here as necessary
+
+# Mapping of node identifiers to their respective class implementations
+NODE_CLASS_MAPPINGS = {
+    "concat": concat,
+    "Filter": Filter,
+    "PresentString": PresentString,
+    "SizeSelector": SizeSelector,
+    "SingleString": SingleString,
+    # You can add more node mappings here
+}
+
+# Mapping of node identifiers to their display names for UI or logging purposes
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "concat": "Concat String (Klinter)",
+    "Filter": "Filter (Klinter)",
+    "PresentString": "Show String (klinter)",
+    "SizeSelector": "Size Selector (klinter)",
+    "SingleString": "Single String (klinter)",
+    # Additional display names can be added here
+}
+
+# Define what symbols this module exports
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
